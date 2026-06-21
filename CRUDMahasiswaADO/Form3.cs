edbcaf31369328a6c99a72ce74a04ff4247005bc
Razfar14 <rajarangkuty14@gmail.com>
2026-06-21 23:45:39 +0700

    ini bagian cetak

---
+++ CRUDMahasiswaADO/Form3.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class Form3: Form
+    {
+        DAL dbLogic = new DAL();
+
+        static string connectionString = "Data Source=LAPTOP-6UCOLCI3\\RAZFAR;Initial Catalog=DBAkademikADO;User ID=sa;Password=RajaZhafar2502";
+
+        SqlConnection conn = new SqlConnection(connectionString);
+        SqlDataAdapter da;
+        DataTable dtMahasiswa;
+
+        CrystalReport1 listMahasiswa = new CrystalReport1();
+
+        string prodi { get; set; }
+        DateTime tglmasuk { get; set; }
+        public Form3(string Prodi, DateTime TglMasuk)
+        {
+            InitializeComponent();
+
+            prodi = Prodi;
+            tglmasuk = TglMasuk;
+
+            try
+            {
+                DataTable dtMahasiswa = dbLogic.getDataRekap(prodi, tglmasuk);
+
+                listMahasiswa.SetDataSource(dtMahasiswa);
+                crystalReportViewer1.ReportSource = listMahasiswa;
+                crystalReportViewer1.Refresh();
+            }
+            catch (Exception ex)
+            {
+                //simpanLog(ex.Message);
+                MessageBox.Show("Gagal load data: " + ex.Message);
+            }
+
+
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
